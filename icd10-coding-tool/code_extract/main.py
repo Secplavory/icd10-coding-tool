68ed4452c707771cc4df61aedadd569ef7194bbe
Secplavory <zxcvbnm9996666@gmail.com>
2020-09-25 01:59:20 +0800

    完成排序

--- icd10-coding-tool/code_extract/main.py
+++ icd10-coding-tool/code_extract/main.py
@@ -1,4 +1,5 @@
 import json
+from typing import Counter
 import nltk
 from nltk.tokenize.api import TokenizerI
 tokenizer = nltk.RegexpTokenizer(r"\w+")
@@ -27,49 +28,106 @@ def findCode(codenet_dict, input_str):
     code_dict = {}
     removed_word = {}
     for code, code_info in codenet_dict.items():
-        hasWord = code_info['title'].split(" ", -1)
-        hasWord.extend(code_info['synonyms'])
-        for word in set(hasWord):
-            if code not in code_dict:
-                code_dict[code] = {}
-                code_dict[code]['f'] = 0
-                code_dict[code]['max_p'] = 0
+        titles = set(code_info['title'].split(" ", -1))
+        synonyms = set(code_info['synonyms']).difference(titles)
+        if code not in code_dict:
+            code_dict[code] = {}
+            code_dict[code]['f'] = 0
+            code_dict[code]['max_p'] = 0
+        if code not in removed_word:
+            removed_word[code] = set()
+        # if code == "A01":
+        #     print(titles)
+        #     print(synonyms)
+        #     print(word_rank)
+        for word in titles:
             if word in word_rank:
                 if code_dict[code]['max_p'] < word_rank[word]['p']:
                     code_dict[code]['max_p'] = word_rank[word]['p']
-                code_dict[code]['f'] +=1
-                if code not in removed_word:
-                    removed_word[code] = set()
+                code_dict[code]['f'] += 1
                 removed_word[code].add(word)
-                
-            
-    max_f = 0
-    for code_v in code_dict.values():
-        if max_f < code_v['f']:
-            max_f = code_v['f']
+
+        for word in synonyms:
+            if word in word_rank:
+                if code_dict[code]['max_p'] < word_rank[word]['p']:
+                    code_dict[code]['max_p'] = word_rank[word]['p']
+                code_dict[code]['f'] += 0.5
+                removed_word[code].add(word)
+
     for code, code_info in codenet_dict.items():
-        next_total_p = 0
         if 'child' in codenet_dict[code]:
-            if code in removed_word:
-                tmp_removed = removed_word[code]
-            else:
-                tmp_removed = set()
-            nextCode_dict = findCode(codenet_dict[code]['child'], " ".join(set(word_rank).difference(tmp_removed)))
+            nextCode_dict = findCode(codenet_dict[code]['child'], " ".join([word for word in input_str.split(" ", -1) if word not in removed_word[code]]))
             code_dict[code]['child'] = nextCode_dict
-            for next_code_info in nextCode_dict.values():
-                if next_code_info['p'] > 0:
-                    next_total_p += next_code_info['p']
-            
-        if max_f != 0:
-            code_dict[code]['w'] = code_dict[code]['f'] / max_f
-            code_dict[code]['p'] = code_dict[code]['max_p'] * code_dict[code]['w']
-            code_dict[code]['p'] = code_dict[code]['p'] + next_total_p
-        else:
-            code_dict[code]['w'] = 0
-            code_dict[code]['p'] = 0 + next_total_p
+        #     for next_code_info in nextCode_dict.values():
+        #         if next_code_info['p'] > 0:
+        #             next_total_p += next_code_info['p']
+        #             next_count += 1
+        # if next_count > 0:
+        #     next_total_p = (next_total_p / next_count) * 0.8
+        # if max_f != 0:
+        #     code_dict[code]['w'] = code_dict[code]['f'] / max_f
+        #     code_dict[code]['p'] = code_dict[code]['max_p'] * code_dict[code]['w']
+        #     code_dict[code]['p'] = code_dict[code]['p'] + next_total_p
+
+        # else:
+        #     code_dict[code]['w'] = 0
+        #     code_dict[code]['p'] = 0 + next_total_p
 
     return code_dict
 
+def calculate_max_f(candidate, count=0):
+    for code_info in candidate.values():
+        if count < code_info['f']:
+            count = code_info['f']
+        if "child" in code_info:
+            count = calculate_max_f(code_info['child'], count)
+    return count
+
+def calculate_w(candidate, max_f):
+    for code_info in candidate.values():
+        if max_f > 0:
+            code_info['w'] = code_info['f'] / max_f
+        else:
+            code_info['w'] = 0
+        if "child" in code_info:
+            calculate_w(code_info['child'], max_f)
+    return candidate
+
+def calculate_P(candidate):
+    total_p = 0
+    code_len = 0
+    for code_info in candidate.values():
+        code_info['p'] = code_info['w'] * code_info['max_p']
+        if "child" in code_info:
+            p = calculate_P(code_info['child'])
+            # code_info['p'] += p
+        if code_info['p'] > 0:
+            code_len += 1
+            total_p += code_info['p']
+    if code_len > 0:
+        return (total_p / code_len)
+    else:
+        return 0
+
+def getMax_p(candidate):
+    max_p = 0
+    for code_info in candidate.values():
+        if max_p < code_info['p']:
+            max_p = code_info['p']
+        if "child" in code_info:
+            p = getMax_p(code_info['child'])
+            if max_p < p:
+                max_p = p
+    return max_p
+
+def printCandidate(candidate, count=0):
+    for k,v in sorted(candidate.items(), key=lambda x:x[1]['p'], reverse=True):
+        if v['p'] > 0 :
+            for i in range(0, count):
+                print("\t", end="")
+            print(k, "：", v['p'])
+            if "child" in v:
+                printCandidate(v['child'], count+1)
 
 if __name__ == "__main__":
     with open("../data/icd10/icd10_codenet.json", 'r') as f:
@@ -77,8 +135,13 @@ if __name__ == "__main__":
     input_str = input("輸入病摘：")
     print()
     candidate = findCode(codenet_dict, input_str)
-    
-    
-    for k,v in sorted(candidate.items(), key=lambda x:x[1]['p'], reverse=True):
-        if v['p'] > 0 :
-            print(k, v['p'])
+    max_f = calculate_max_f(candidate)
+    candidate = calculate_w(candidate, max_f)
+    calculate_P(candidate)
+    for code_info in candidate.values():
+        if 'child' in code_info:
+            max_p = getMax_p(code_info['child'])
+            if code_info['p'] < max_p:
+                code_info['p'] = max_p
+    print(max_f)
+    printCandidate(candidate)
